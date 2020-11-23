3e41c8bb5397db1aaf9a6257e35d4895fc535d17
Neo-Techies <mbcse50@gmail.com>
2020-11-24 01:52:08 +0530

    Completed Excercise with all test cases passed

--- truffle.js
+++ truffle.js
@@ -2,13 +2,13 @@ module.exports = {
   compilers: {
     solc: {
       version: "0.6.12",
-    }
+    },
   },
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: "*", // Match any network id
+    },
+  },
 };
